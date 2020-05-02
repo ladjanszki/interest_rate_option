4848a7f5803548aa894aab90e7624dda7de9f64c
István Ladjánszki <istvan.ladjanszki@gmail.com>
2020-05-02 13:48:57 +0200

    * actual vasicek code

--- vasicek.py
+++ vasicek.py
@@ -3,11 +3,13 @@ This file is the entry point of the interest rate option pricing
 based on the Vasicek model
 '''
 
+import numpy as np
+
 import util
 
 
 # Parameters
-T = 5 # In years
+T = 7 # In years
 dt = 0.5 # Time step
 #nLevel = int(T / dt)
 
@@ -21,8 +23,31 @@ r0 = 0.046 # 4.6%
 # Strike
 K = 0.047 # 4.7% 
 
+# Forward rates
+forward = np.zeros(15, dtype=float) 
+forward[0] = 0.045
+forward[1] = 0.050
+forward[2] = 0.055
+forward[3] = 0.045
+forward[4] = 0.040
+forward[5] = 0.045
+forward[6] = 0.048
+forward[7] = 0.050
+forward[8] = 0.050
+forward[9] = 0.044
+forward[10] = 0.045 
+forward[11] = 0.048
+forward[12] = 0.045
+forward[13] = 0.042
+forward[14] = 0.043
+ 
 
-tree = util.Tree(T, dt, k, theta, sigma)
+# Zero coupon curve
+#zeroCoupon = np.cumsum(forward).tolist()
+zeroCoupon = np.cumsum(forward).tolist()
+
+
+tree = util.Tree(T, dt, k, theta, sigma, zeroCoupon)
 
 tree.visualize('pdf')
 
